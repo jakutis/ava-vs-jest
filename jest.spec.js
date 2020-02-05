7f58222a77a71e396dc1c3a02f9e5b0c3c87fa99
Vytautas Jakutis <jakutis@wix.com>
2020-02-05 20:21:28 +0200

    jest test more similar

--- jest.spec.js
+++ jest.spec.js
@@ -1,6 +1,6 @@
-for (var i = 1; i <= Number(process.env.FATJEST_COUNT); i++) {
-  describe('product #' + i, () => {
-    test('works', () => {
+describe('product', () => {
+  for (var i = 1; i <= Number(process.env.FATJEST_COUNT); i++) {
+    test('works #' + i, () => {
     })
-  })
-}
+  }
+})
